69c599cffe9af662a04ccf456c1a34f2ec71a8ac
Ketrofan <ketrifiko@gmail.com>
2024-01-20 19:01:01 +0300

    calchtml

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <input id="a"\>
-        <div id="operator-btns">
-            <button id="plus" onclick="op='+'">+</button>
-            <button id="minus" onclick="op='-'">-</button>
-            <button id="times" onclick="op='*'">*</button>
-            <button id="divide" onclick="op='/'">/</button>
-        </div>
-    <input id="b"\>
-    <button onclick="func()">Решить</button>
-    <p id="result"></p>
-    <script>
 var op;
     function func(){
         var result;
@@ -40,6 +21,3 @@ var op;
     document.getElementById("result").innerHTML = result;    
 
     }
-    </script>
-</body>
-</html>
